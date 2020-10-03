e788ac1e69652f99ad22843c5b4b68d64734c992
ElMasDuroDeLaNueva <72275590+ElMasDuroDeLaNueva@users.noreply.github.com>
2020-10-03 18:35:55 +0200

    Primeras líneas muy decepcionantes

--- src/PerfilUsuario.java
+++ src/PerfilUsuario.java
@@ -1,5 +1,47 @@
-public class PerfilUsuario {
-    public class main(String args[]){
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
+public class PerfilUsuario {
+    public static void main(String args[]){
+        //Frame principal
+        JFrame framePerfil = new JFrame("MyBasket");
+        framePerfil.setLayout(new BorderLayout());
+        framePerfil.setSize(850, 600);
+
+        //Botones compra, lista, nevera
+        JButton botonCompra = new JButton("Compra");
+        JButton botonLista = new JButton("Mi Lista");
+        JButton botonMiNevera = new JButton("Mi nevera");
+        botonCompra.addActionListener(new BotonesMenu());
+        botonLista.addActionListener(new BotonesMenu());
+        botonMiNevera.addActionListener(new BotonesMenu());
+
+        //Paneles
+        JPanel panelNorth = new JPanel();
+        JPanel panelSouth = new JPanel();
+        JPanel panelCenter = new JPanel();
+        JPanel panelMyBasket = new JPanel();
+
+        //Definición de componentes
+        panelNorth.setLayout(new BorderLayout());
+        panelSouth.setLayout(new BorderLayout());
+        //panelMyBasket.setLayout(new FlowLayout());
+        panelCenter.setLayout(new GridLayout(0, 3));
+
+        //Anidación de paneles
+        //panelMyBasket.add("MyBasket");
+        //panelSouth.add(panelMyBasket, BorderLayout.CENTER);
+        panelCenter.add(botonCompra, BorderLayout.EAST);
+        panelCenter.add(botonLista, BorderLayout.CENTER);
+        panelCenter.add(botonMiNevera, BorderLayout.WEST);
+
+        //Definición frame
+        framePerfil.add(panelNorth, BorderLayout.NORTH);
+        framePerfil.add(panelSouth, BorderLayout.SOUTH);
+        framePerfil.add(panelCenter, BorderLayout.CENTER);
+        framePerfil.setLocationRelativeTo(null);
+        framePerfil.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        framePerfil.setVisible(true);
     }
 }
